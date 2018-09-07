be9599f7f72d8e6a2fff8b0544ae9acd3ca6aa63
LiNk-NY <marcel.ramosperez@roswellpark.org>
2018-09-07 16:34:47 +0000

    update valid to version in Risa-biocViews.R

--- R/Risa-biocViews.R
+++ R/Risa-biocViews.R
@@ -2,7 +2,7 @@ bioconductor.version <- 3.1
 
 getPackagesInBiocView <- function(view, 
                                   reposUrl = c("BiocSoftware", "BiocAnnotationData", "BiocExperimentData"),
-                                  valid = bioconductor.version) {
+                                  version = bioconductor.version) {
         
     data("biocViewsVocab")
     
@@ -19,7 +19,7 @@ getPackagesInBiocView <- function(view,
     
     reposUrl <- paste(biocMirror,
                  "packages",
-                 valid,
+                 version,
                  biocPaths, 
                  sep = "/") 
     
